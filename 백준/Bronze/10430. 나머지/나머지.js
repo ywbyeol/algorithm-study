8cbfc2a0d1c7eb10950fd2382f3111b1e272475a
ywbyeol <ywbyeol05@gmail.com>
2022-12-30 14:31:31 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(" ");
+const A = Number(input[0]);
+const B = Number(input[1]);
+const C = Number(input[2]);
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
