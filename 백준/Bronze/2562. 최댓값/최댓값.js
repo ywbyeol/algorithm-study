f60430a62a91a18c16166189532583ff78c4eb95
ywbyeol <ywbyeol05@gmail.com>
2023-01-09 23:36:26 +0900

    [Bronze III] Title: 최댓값, Time: 148 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+const max = Math.max(...input);
+console.log(`${max}\n${input.indexOf(max) + 1}`);
