5733597425f28658848ba25a05a1f015a19de2c5
ywbyeol <ywbyeol05@gmail.com>
2023-01-11 03:40:18 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2480 ms, Memory: 246932 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const input = fs
-  .readFileSync('/dev/stdin')
-  .toString()
-  .trim()
-  .split('\n')
-  .map(BigInt);
-const input_copy = [...input];
-input_copy.shift();
-input_copy.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
-const input_sorted = input_copy.join('\n');
-console.log(input_sorted);
+  const fs = require('fs');
+  const input = fs
+    .readFileSync('/dev/stdin')
+    .toString()
+    .trim()
+    .split('\n')
+    .map(BigInt);
+  const input_copy = [...input];
+  input_copy.shift();
+  input_copy.sort((a, b) => (a < b ? -1 : a > b ? 1 : 0));
+  const input_sorted = input_copy.join('\n');
+  console.log(input_sorted);
