c94301213e5056d006cfe3c2b7ee73464fe42cc7
ywbyeol <ywbyeol05@gmail.com>
2023-01-22 11:04:48 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.js
+++ 백준/Bronze/2908. 상수/상수.js
@@ -1,5 +1,14 @@
 const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().trim();
-const num1 = input[2] + input[1] + input[0];
-const num2 = input[6] + input[5] + input[4];
-console.log(num1 > num2 ? num1 : num2);
+console.log(
+  Math.max(
+    ...fs
+      .readFileSync('/dev/stdin')
+      .toString()
+      .trim()
+      .split('')
+      .reverse()
+      .join('')
+      .split(' ')
+      .map(Number),
+  ),
+);
