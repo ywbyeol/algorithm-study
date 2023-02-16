69193a07a39b39e3f16aa62052c892cd94c75b42
ywbyeol <ywbyeol05@gmail.com>
2023-02-16 23:42:57 +0900

    [Bronze V] Title: 몇개고?, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/27294. 몇개고？/몇개고？.js
+++ 백준/Bronze/27294. 몇개고？/몇개고？.js
@@ -1,16 +1,8 @@
 const fs = require('fs');
 const [T, S] = fs
-  .readFileSync('dev/stdin')
+  .readFileSync('/dev/stdin')
   .toString()
   .trim()
   .split(' ')
   .map(Number);
-let rice = 0;
-if (S === 1) {
-  rice = 280;
-} else if (T >= 12 && T <= 16) {
-  rice = 320;
-} else {
-  rice = 280;
-}
-console.log(rice);
+console.log(S === 1 || !(T >= 12 && T <= 16) ? 280 : 320);
