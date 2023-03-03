180759e764fdcb19e4794e051a6f6ff1691f9ca2
ywbyeol <ywbyeol05@gmail.com>
2023-03-03 19:40:24 +0900

    [Bronze I] Title: 평균, Time: 136 ms, Memory: 9772 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -1,6 +1,5 @@
 process.stdin.on('data', i => {
-  const [N, input] = i.toString().trim().split('\n');
-  const scores = input.split(' ').map(Number);
+  const [N, ...scores] = i.toString().trim().split(/\s/).map(Number);
   const M = Math.max(...scores);
   console.log(scores.reduce((a, b) => a + (b / M) * 100, 0) / Number(N));
 });
