20a7ab92b5805c5246bbe4880267168db41dfaa0
ywbyeol <ywbyeol05@gmail.com>
2023-03-08 18:35:25 +0900

    [Bronze V] Title: Робинзон Крузо, Time: 128 ms, Memory: 9388 KB -BaekjoonHub

--- 백준/Bronze/27219. Робинзон Крузо/Робинзон Крузо.js
+++ 백준/Bronze/27219. Робинзон Крузо/Робинзон Крузо.js
@@ -1,4 +1,4 @@
 process.stdin.on('data', i => {
-  const num = Number(i.toString());
-  console.log(`${'V'.repeat(Math.floor(num / 5))}${'I'.repeat(num % 5)}`);
+  const num = Number(i.toString().trim());
+  console.log('V'.repeat(Math.floor(num / 5)) + 'I'.repeat(num % 5));
 });
