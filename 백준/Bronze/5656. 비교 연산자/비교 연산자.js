99f1b4c1caed960b08f0b3844c4629db9973afea
ywbyeol <ywbyeol05@gmail.com>
2023-06-07 00:06:49 +0900

    [Bronze II] Title: 비교 연산자, Time: 2452 ms, Memory: 22416 KB -BaekjoonHub

--- 백준/Bronze/5656. 비교 연산자/비교 연산자.js
+++ 백준/Bronze/5656. 비교 연산자/비교 연산자.js
@@ -1,4 +1,3 @@
-let N = 1;
 let I = `${require('fs').readFileSync(0)}`.trim().split(`\n`);
 for (let i = 0; i < I.length; i++) if (I[i].includes('E')) I = I.slice(0, i);
-console.log(I.map(v => `Case ${N++}: ${eval(v)}`).join('\n'));
+console.log(I.map((v, i) => `Case ${i + 1}: ${eval(v)}`).join('\n'));
