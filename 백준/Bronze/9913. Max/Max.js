1ec7b7d992e2273a211f32e644d6b983d3d8f898
ywbyeol <ywbyeol05@gmail.com>
2023-07-10 09:27:17 +0900

    [Bronze II] Title: Max, Time: 124 ms, Memory: 9652 KB -BaekjoonHub

--- 백준/Bronze/9913. Max/Max.js
+++ 백준/Bronze/9913. Max/Max.js
@@ -1,7 +1,3 @@
 const m = new Map();
-`${require('fs').readFileSync(0)}`.trim().split('\n').slice(1).forEach(v => m.set(v, m.has(v) ? +m.get(v) + 1 : 1));
-let a = 0;
-m.forEach(v => {
-  if (v > a) a = v;
-});
-console.log(a);
+`${require('fs').readFileSync(0)}`.trim().split('\n').slice(1).forEach(v => m.set(v, (m.get(v) || 0) + 1));
+console.log(Math.max(...m.values()));
