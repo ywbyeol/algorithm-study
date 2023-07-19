d0c510da404e8b093c6b230ae105b8d4a2f3621c
ywbyeol <ywbyeol05@gmail.com>
2023-07-19 11:08:53 +0900

    [Bronze II] Title: 문어 숫자, Time: 124 ms, Memory: 9640 KB -BaekjoonHub

--- 백준/Bronze/1864. 문어 숫자/문어 숫자.js
+++ 백준/Bronze/1864. 문어 숫자/문어 숫자.js
@@ -1,7 +1,7 @@
-const cheat = { '-': 0, '\\': 1, '(': 2, '@': 3, '?': 4, '>': 5, '&': 6, '%': 7, '/': -1 };
-const input = `${require('fs').readFileSync(0)}`
+const c = { '-': 0, '\\': 1, '(': 2, '@': 3, '?': 4, '>': 5, '&': 6, '%': 7, '/': -1 };
+const o = `${require('fs').readFileSync(0)}`
   .trim()
   .split(/\s+/)
   .slice(0, -1)
-  .map(v => [...v].map((x, i, a) => cheat[x] * 8 ** (a.length - i - 1)).reduce((a, b) => a + +b, 0));
-console.log(input.join('\n'));
+  .map(v => [...v].map((x, i, a) => c[x] * 8 ** (a.length - i - 1)).reduce((a, b) => a + +b, 0));
+console.log(o.join('\n'));
