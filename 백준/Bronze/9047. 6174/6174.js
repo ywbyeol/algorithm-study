39ce8f25d9b692c2b84c67cd962a0dd17fdf2d13
ywbyeol <ywbyeol05@gmail.com>
2023-07-24 15:42:54 +0900

    [Bronze I] Title: 6174, Time: 128 ms, Memory: 10768 KB -BaekjoonHub

--- 백준/Bronze/9047. 6174/6174.js
+++ 백준/Bronze/9047. 6174/6174.js
@@ -1,10 +1,7 @@
 const K = n => {
-  let t = n.trim();
-  let c = 0;
-  while (t !== '6174') {
-    c++;
-    t = `${+[...t].sort().reverse().join('') - +[...t].sort().join('')}`.trim().padStart(4, '0');
-  }
-  return c;
+  const i = (t, c) =>
+    t === '6174' ? c : i(`${+[...t].sort().reverse().join('') - +[...t].sort().join('')}`.padStart(4, '0'), c + 1);
+  const t = n.trim();
+  return i(t, 0);
 };
 console.log(`${require('fs').readFileSync(0)}`.trim().split('\n').slice(1).map(K).join('\n'));
