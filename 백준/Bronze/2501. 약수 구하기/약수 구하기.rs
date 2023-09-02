282df1bda090e7dd5b4e3a86f4596dbeabb45b4a
ywbyeol <ywbyeol05@gmail.com>
2023-09-03 07:22:00 +0900

    [Bronze III] Title: 약수 구하기, Time: 4 ms, Memory: 13148 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.rs
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.rs
@@ -3,11 +3,10 @@ use std::io::{stdin, Read};
 fn main() {
     let mut s = String::new();
     stdin().read_to_string(&mut s).unwrap();
-    let v: Vec<u32> = s.split_whitespace().map(|x| x.parse().unwrap()).collect();
-    let f: Vec<u32> = (1..=v[0]).filter(|&x| v[0] % x == 0).collect();
-    if v[1] as usize > f.len() {
-        println!("0");
-    } else {
-        println!("{}", f[v[1] as usize - 1]);
-    }
+    let v: Vec<usize> = s.split_whitespace().map(|x| x.parse().unwrap()).collect();
+    let f: usize = (1..=v[0])
+        .filter(|&x| v[0] % x == 0)
+        .nth(v[1] - 1)
+        .unwrap_or(0);
+    println!("{}", f);
 }
