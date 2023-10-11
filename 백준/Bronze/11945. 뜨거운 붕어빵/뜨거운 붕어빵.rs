e13fa8725725ea4131eb6ca2e2eb07c08884e0a8
ywbyeol <ywbyeol05@gmail.com>
2023-10-11 21:45:20 +0900

    [Bronze IV] Title: 뜨거운 붕어빵, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.rs
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.rs
@@ -1,8 +1,6 @@
 fn main() {
     let i = std::io::stdin().lines().skip(1);
-    i.map(|l| l.unwrap()).for_each(|v| {
-        let mut v = v.split("").collect::<Vec<_>>();
-        v.reverse();
-        println!("{}", v.join(""));
+    i.for_each(|v| {
+        println!("{}", v.unwrap().chars().rev().collect::<String>());
     });
 }
