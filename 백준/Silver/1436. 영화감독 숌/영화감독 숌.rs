794d871f9769b3be2cf8e4efdc33792c9f8aa1e4
ywbyeol <ywbyeol05@gmail.com>
2023-11-02 15:05:49 +0900

    [Silver V] Title: 영화감독 숌, Time: 36 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.rs
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.rs
@@ -1,5 +1,21 @@
 fn main() {
     let n = std::io::read_to_string(std::io::stdin()).unwrap();
-    let mut i = (666..2666800).filter(|v| v.to_string().contains("666"));
+    let mut i = (666..2666800).filter(|v| f(v));
     println!("{}", i.nth(n.trim().parse::<usize>().unwrap() - 1).unwrap());
+}
+
+fn f(n: &i32) -> bool {
+    let (mut c, mut s) = (*n, 0);
+    while c > 0 {
+        if c % 10 == 6 {
+            s += 1;
+            if s == 3 {
+                return true;
+            }
+        } else {
+            s = 0;
+        }
+        c /= 10;
+    }
+    false
 }
