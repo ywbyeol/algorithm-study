08ea96dd613411732c26386880d4a5704148eebd
ywbyeol <ywbyeol05@gmail.com>
2023-11-05 13:20:07 +0900

    [Gold IV] Title: 문자열 폭발, Time: 120 ms, Memory: 19316 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.rs
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.rs
@@ -3,13 +3,18 @@ fn main() {
     let (s, e, mut v) = (l(), l(), Vec::new());
     for c in s.chars() {
         v.push(c);
-        if v.ends_with(&Vec::from_iter(e.chars())) {
-            v.drain(v.len() - e.len()..);
+        if v.len() >= e.len() {
+            let t: String = v.iter().skip(v.len() - e.len()).collect();
+            if t == e {
+                for _ in 0..e.len() {
+                    v.pop();
+                }
+            }
         }
     }
     if v.is_empty() {
         println!("FRULA");
     } else {
-        println!("{}", String::from_iter(v.into_iter()));
+        println!("{}", v.into_iter().collect::<String>());
     }
 }
