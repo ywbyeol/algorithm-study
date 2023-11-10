c2f00ceb917c2221c91317bd93163a9ce76b7888
ywbyeol <ywbyeol05@gmail.com>
2023-11-10 15:04:25 +0900

    [Gold V] Title: 줄어드는 수, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Gold/1174. 줄어드는 수/줄어드는 수.rs
@@ -0,0 +1,16 @@
+use std::collections::VecDeque;
+
+fn main() {
+    let n = std::io::read_to_string(std::io::stdin()).unwrap();
+    let (n, mut a) = (n.trim().parse().unwrap(), 0);
+    let mut d = VecDeque::from_iter(0_i64..if n > 1023 { 0 } else { 10 });
+    while let Some(c) = d.pop_front() {
+        a += 1;
+        if a == n {
+            println!("{}", c);
+            return;
+        }
+        (0..(c % 10)).for_each(|i| d.push_back(c * 10 + i));
+    }
+    println!("-1");
+}
