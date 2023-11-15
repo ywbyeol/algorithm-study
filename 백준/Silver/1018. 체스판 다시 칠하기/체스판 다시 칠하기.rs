fb5ec89c798ad39e75d8e548586ec2ab98d89753
ywbyeol <ywbyeol05@gmail.com>
2023-11-16 06:37:54 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.rs
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.rs
@@ -1,10 +1,10 @@
 fn main() {
     let mut l = std::io::stdin().lines().flatten();
-    let v = l.next().unwrap();
+    let (mut r, v) = (32, l.next().unwrap());
     let v = Vec::from_iter(v.split(' ').flat_map(str::parse::<usize>));
     let b = Vec::from_iter(l.map(|l| l.chars().collect()));
-    let r = (0..(v[0] - 7)).flat_map(|i| (0..(v[1] - 7)).map(move |j| (i, j)));
-    println!("{}", r.map(|(i, j)| f(&b, i, j)).min().unwrap());
+    (0..(v[0] - 7)).for_each(|i| (0..(v[1] - 7)).for_each(|j| r = r.min(f(&b, i, j))));
+    println!("{}", r);
 }
 
 fn f(b: &[Vec<char>], x: usize, y: usize) -> u8 {
