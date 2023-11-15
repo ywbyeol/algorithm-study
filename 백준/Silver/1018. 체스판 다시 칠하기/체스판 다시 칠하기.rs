7988bb53a4f313fab3f77402cdb202bbcfa74e7e
ywbyeol <ywbyeol05@gmail.com>
2023-11-16 06:21:19 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 4 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.rs
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.rs
@@ -1,16 +1,22 @@
 fn main() {
     let mut l = std::io::stdin().lines().flatten();
-    let (v, a) = (l.next().unwrap(), Vec::from_iter("WB".repeat(4).chars()));
-    let v = Vec::from_iter(v.split(' ').flat_map(str::parse));
-    let (b, mut r) = (Vec::from_iter(a.iter().rev().cloned()), 32);
-    let c = Vec::from_iter((1..9).map(|i| if i % 2 == 1 { &a } else { &b }.clone()));
-    let b = Vec::from_iter(l.take(v[0]).map(|l| l.chars().collect()));
-    (0..(v[0] - 7)).for_each(|i| (0..(v[1] - 7)).for_each(|j| r = r.min(f(&c, &b, i, j))));
-    println!("{}", r);
+    let v = l.next().unwrap();
+    let v = Vec::from_iter(v.split(' ').flat_map(str::parse::<usize>));
+    let b = Vec::from_iter(l.map(|l| l.chars().collect()));
+    let r = (0..(v[0] - 7)).flat_map(|i| (0..(v[1] - 7)).map(move |j| (i, j)));
+    println!("{}", r.map(|(i, j)| f(&b, i, j)).min().unwrap());
 }
 
-fn f(c: &[Vec<char>], b: &[Vec<char>], x: usize, y: usize) -> u8 {
-    let mut r = 0;
-    (0..8).for_each(|i| (0..8).for_each(|j| r += if c[i][j] == b[x + i][y + j] { 1 } else { 0 }));
-    r.min(64 - r)
+fn f(b: &[Vec<char>], x: usize, y: usize) -> u8 {
+    let mut c = 0;
+    for i in 0..8 {
+        for j in 0..8 {
+            if (x + i + y + j) % 2 == 0 {
+                c += if b[x + i][y + j] == 'B' { 1 } else { 0 }
+            } else if b[x + i][y + j] == 'W' {
+                c += 1;
+            }
+        }
+    }
+    c.min(64 - c)
 }
