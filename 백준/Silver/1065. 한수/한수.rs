6c52cf3851632ccddc2d2b6f6e3642adbbae18d3
ywbyeol <ywbyeol05@gmail.com>
2023-11-17 08:21:44 +0900

    [Silver IV] Title: 한수, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.rs
+++ 백준/Silver/1065. 한수/한수.rs
@@ -1,18 +1,5 @@
 fn main() {
     let n = std::io::read_to_string(std::io::stdin()).unwrap();
-    print!("{}", (1..=n.trim().parse().unwrap()).filter(f).count());
-}
-
-fn f(i: &u16) -> bool {
-    let (mut i, mut d) = (*i, i % 10);
-    i /= 10;
-    d -= i % 10;
-    while i >= 10 {
-        let l = i % 10;
-        i /= 10;
-        if l - i % 10 != d {
-            return false;
-        }
-    }
-    true
+    let c = (1..=n.trim().parse().unwrap()).filter(|&i| i < 100 || i / 100 * 21 + i == i / 10 * 12);
+    print!("{}", c.count());
 }
