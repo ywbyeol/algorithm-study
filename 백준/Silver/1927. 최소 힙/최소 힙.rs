9d770787821fd6c4de52cdb3f7e785879bc82294
ywbyeol <ywbyeol05@gmail.com>
2023-11-27 09:08:09 +0900

    [Silver II] Title: 최소 힙, Time: 20 ms, Memory: 13724 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.rs
@@ -0,0 +1,14 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::io::{self, Write};
+
+fn main() {
+    let (mut h, l) = (BinaryHeap::new(), io::stdin().lines().skip(1));
+    let mut o = io::BufWriter::new(io::stdout().lock());
+    for l in l.flat_map(|v| v.unwrap().parse()) {
+        match l {
+            0 => writeln!(o, "{}", h.pop().unwrap_or(Reverse(0)).0).unwrap(),
+            _ => h.push(Reverse(l)),
+        }
+    }
+}
