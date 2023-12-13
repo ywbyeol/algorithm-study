a58fcd0defc862fa6b902903228133c47b22719d
ywbyeol <ywbyeol05@gmail.com>
2023-12-14 06:33:19 +0900

    [Silver III] Title: 파일 정리, Time: 72 ms, Memory: 24144 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.rs
+++ 백준/Silver/20291. 파일 정리/파일 정리.rs
@@ -1,10 +1,14 @@
 use std::collections::HashMap;
+use std::io::{self, Write};
 
 fn main() {
     let s = std::io::read_to_string(std::io::stdin()).unwrap();
     let (s, mut h) = (s.split_whitespace().skip(1), HashMap::new());
     s.for_each(|l| *h.entry(l.split('.').nth(1).unwrap()).or_insert(0) += 1);
     let mut h = Vec::from_iter(h);
+    let mut o = io::BufWriter::new(io::stdout());
     h.sort_by_key(|(n, _)| *n);
-    h.iter().for_each(|e| println!("{} {}", e.0, e.1));
+    for e in h {
+        writeln!(o, "{} {}", e.0, e.1).unwrap();
+    }
 }
