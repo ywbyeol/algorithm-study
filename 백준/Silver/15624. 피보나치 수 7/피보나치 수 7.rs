5dbf6c9e996e91dc91005d57eb2ff7a015369bfe
ywbyeol <ywbyeol05@gmail.com>
2023-12-24 10:29:33 +0900

    [Silver IV] Title: 피보나치 수 7, Time: 4 ms, Memory: 13204 KB, Score: 201 point -BaekjoonHub

--- 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.rs
+++ 백준/Silver/15624. 피보나치 수 7/피보나치 수 7.rs
@@ -1,5 +1,5 @@
 fn main() {
     let n = std::io::read_to_string(std::io::stdin()).unwrap();
-    let n = (2..=n.trim().parse().unwrap()).fold([0, 1], |m, _| [m[1], (m[0] + m[1]) % 1000000007]);
-    print!("{}", n[1]);
+    let r = (2..=n.trim().parse().unwrap()).fold([0, 1], |m, _| [m[1], (m[0] + m[1]) % 1000000007]);
+    print!("{}", r[if n.trim() == "0" { 0 } else { 1 }]);
 }
