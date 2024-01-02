b4cc844e92e74adeb4d7c77f85bc403e735a2df9
ywbyeol <ywbyeol05@gmail.com>
2024-01-02 13:15:06 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 4 ms, Memory: 13352 KB -BaekjoonHub

--- 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.rs
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.rs
@@ -4,10 +4,9 @@ fn main() {
     let s = std::io::read_to_string(std::io::stdin()).unwrap();
     let mut h = HashSet::from(["ChongChong"]);
     for l in s.lines().skip(1) {
-        let (a, b) = l.split_once(" ").unwrap();
+        let (a, b) = l.split_once(' ').unwrap();
         if h.get(a).is_some() || h.get(b).is_some() {
-            h.insert(a);
-            h.insert(b);
+            (h.insert(a), h.insert(b));
         }
     }
     print!("{}", h.len());
